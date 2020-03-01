eb06fd801bf8b5fdd00f5d85196db51144fa5f9e
Ville Marttila <ville.marttila@iki.fi>
2020-03-02 01:32:09 +0200

    Tehtävä 6.8 tehty

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { vote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/anecdoteForm'
+import AnecdoteList from './components/anecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((a,b) => b.votes - a.votes))
-  const dispatch = useDispatch()
-
   return (
     <div>
       <h1>Anecdotes</h1>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-      
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
