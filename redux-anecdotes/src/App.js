f5062e7ec93560b2323729aa99806dec586a00fe
Ville Marttila <ville.marttila@iki.fi>
2020-03-02 21:46:53 +0200

    Tehtävä 6.15 tehty

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -9,10 +9,10 @@ import { useDispatch } from 'react-redux'
 
 const App = () => {
 const dispatch = useDispatch()
+
 useEffect(() => {
-  anecdoteService
-    .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
-}, [dispatch])
+  dispatch(initializeAnecdotes()) 
+},[dispatch]) 
 
   return (
     <div>
