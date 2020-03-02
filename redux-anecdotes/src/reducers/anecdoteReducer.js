c9116444c729f19afb7a52dc7117aa2e200b1fbd
Ville Marttila <ville.marttila@iki.fi>
2020-03-02 20:32:06 +0200

    Tehtävä 6.13 tehty

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,4 @@
-const anecdotesAtStart = [
+/*const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -6,7 +6,7 @@ const anecdotesAtStart = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
+*/
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -31,9 +31,16 @@ export const vote = (id) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
+  }
+}
 
-const anecdoteReducer = (state = initialState, action) => {
+// const initialState = anecdotesAtStart.map(asObject)
+
+const anecdoteReducer = (state = [], action) => {
   switch(action.type) {
     case 'VOTE':  
       const id = action.data.id
@@ -48,6 +55,8 @@ const anecdoteReducer = (state = initialState, action) => {
       case 'NEW_ANECDOTE':
         const newAnecdote = asObject(action.data.content)
         return state.concat(newAnecdote)
+      case 'INIT_ANECDOTES':
+        return action.data
     default:
     return state
   }
